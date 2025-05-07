b91b9322544aa91453f23b401845ad0054605f3b
Xaika1 <tsvetkov_igor@internet.ru>
2025-05-07 12:28:36 +0300

    Update hardware_tasks.sql

--- hardware_tasks.sql
+++ hardware_tasks.sql
@@ -1,1 +1,22 @@
 -- 1.
+select sum(amount)
+from hardware;
+-- 2.
+select *
+from hardware
+where amount =0;
+-- 3.
+select avg(price)
+from title
+where name = 'Монитор';
+-- 4.
+select  *
+from hardware
+where title = 'Клавиатура'
+order by price asc;
+-- 5.
+select  count(*) as qwe
+from hardware
+where tag is not null
+group by tag
+order by qwe desc;
